337a02b25bdbfac72400f978d25efa6c549ec4ba
SecreC <gabirocks505@gmail.com>
2023-06-10 21:09:54 -0700

    added search function, moved cs files and uncombined mat dump

---
+++ src/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using static GFDLibrary.Api.FlatApi;
+using GFDLibrary.Materials;
+using GFDLibrary;
+using static P5MatValidator.Comparisons;
+using static P5MatValidator.Validation;
+using static P5MatValidator.Utils;
+using static P5MatValidator.Combine;
+using static P5MatValidator.Dump;
+using static P5MatValidator.Search;
+
+namespace P5MatValidator
+{
+    internal class Program
+    {
+        public static Mode mode = 0;
+        public static int ?matVersion = null;
+        public static Stopwatch Stopwatch = new Stopwatch();
+        public static List<string> FailedMaterialFiles = new List<string>();
+
+        internal enum Mode
+        {
+            validate = 1,
+            strict = 2,
+            dump = 4,
+            combine = 8,
+            search = 16,
+        }
+        internal struct MaterialInfo
+        {
+            internal List<Material> materials;
+            internal string? fileName;
+        }
+        static void ShowProgramUsage()
+        {
+            Console.WriteLine("P5MatValidator By SecreC.\n\n" +
+                    "Usage: <Input model or model directory> <dump directory or output> <Modes>\n" +
+                    "Modes:\n" +
+                    "\t-validate; compare each material in the first arg to the reference materials in the second and return which ones do and don't match\n" +
+                    "\t-strict; values less likely to cause crashes will be compared\n" +
+                    "\t-dump; dumps gmtds of all models in the first arg to the path in the second\n" +
+                    "\t-combine <gfd version number (decimal)>; combines all materials in first arg subdirectories and outputs a single material to the output path\n" +
+                    "\t-search <material dump path> <<material member> <value>>...; search for a material of a specific description\n");
+
+            Console.ReadKey();
+            return;
+        }
+        static void ProcessArgs(string[] args)
+        {
+            //set mode based on args
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-validate") mode |= Mode.validate;
+                if (args[i] == "-strict") mode |= Mode.strict;
+                if (args[i] == "-dump") mode |= Mode.dump;
+                if (args[i] == "-search") mode |= Mode.search;
+                if (args[i] == "-combine")
+                {
+                    mode |= Mode.combine;
+
+                    try
+                    {
+                        matVersion = Int32.Parse(args[i + 1]);
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("no version was found, version won't be filtered");
+                    }
+                    catch(FormatException)
+                    {
+                        Console.WriteLine("version number is not in the correct format. Correct format example: 17846528 (decimal)");
+                        mode = 0;
+                        return;
+                    }
+                    catch(OverflowException)
+                    {
+                        Console.WriteLine("version number is outside the bounds of an integer");
+                        mode = 0;
+                        return;
+                    }
+                }
+            }
+
+            //print program usage if less than 3 args
+            if (args.Length < 3 && ((mode & Mode.search) == 0))
+            {
+                ShowProgramUsage();
+                return;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Process Arguments and set modes
+            ProcessArgs(args);
+
+            //return early if no mode is set
+            if (mode == 0)
+            {
+                ShowProgramUsage();
+                return;
+            }
+
+            //timer for benchmarking
+            Stopwatch.Start();
+
+            //run commands based on 
+            if ((mode & Mode.validate) > 0)
+            {
+                ValidateMaterials(args[0], args[1]);
+                return;
+            }
+            else if ((mode & Mode.combine) > 0)
+            {
+                CreateCombinedMat(args[0], args[1]);
+                return;
+            }
+            else if ((mode & Mode.dump) > 0)
+            {
+                DumpMats(args);
+                return;
+            }
+            else if ((mode & Mode.search) > 0)
+            {
+                SearchForMaterial(args);
+                return;
+            }
+        }
+        internal static async Task<(MaterialInfo, List<MaterialInfo>)> PrepareMaterialLists(string compareModelDir, string referenceMaterialDir)
+        {
+            //Generate Compare Material List 
+            Task<MaterialInfo> compareMaterials = GenerateMaterialList(compareModelDir, referenceMaterialDir);
+
+            //Search all files in refMatDir for mats
+            string[] fileExtenstions = {"*.gmtd", "*.gmt", "*.GFS", "*.GMD"};
+            List<string> matFileNames = GetFiles($"{referenceMaterialDir}", fileExtenstions, SearchOption.AllDirectories);
+
+            List<MaterialInfo> referenceMaterials = new();
+
+            foreach (string matFile in matFileNames)
+            {
+                try
+                {
+                    referenceMaterials.Add(await GenerateMaterialList(matFile, referenceMaterialDir));
+                }
+                catch 
+                {
+                    FailedMaterialFiles.Add(Path.GetRelativePath(referenceMaterialDir, matFile));
+                }
+            }
+
+            return(await compareMaterials, referenceMaterials);
+        }
+
+        internal static async Task<MaterialInfo> GenerateMaterialList(string filePath, string referenceMaterialDir)
+        {
+            MaterialInfo materialInfo = new();
+
+            //Add List of materials and filename to materialInfo struct
+            if (Path.GetExtension(filePath).ToLower() == ".gfs" || Path.GetExtension(filePath).ToLower() == ".gmd")
+            {
+                materialInfo.materials = (List<Material>)LoadModel(filePath).Materials.Materials;
+                materialInfo.fileName = Path.GetRelativePath(referenceMaterialDir, filePath);
+
+                return materialInfo;
+            }
+            else if (Path.GetExtension(filePath).ToLower() == ".gmtd")
+            {
+                var matDict = (MaterialDictionary)Resource.Load(filePath);
+                materialInfo.materials = (List<Material>)matDict.Materials;
+                materialInfo.fileName = Path.GetRelativePath(referenceMaterialDir, filePath);
+
+                return materialInfo;
+            }
+            else if (Path.GetExtension(filePath).ToLower() == ".gmt")
+            {
+                var mat = (Material)Resource.Load(filePath);
+
+                materialInfo.materials = new List<Material>
+                {
+                    mat
+                };
+
+                materialInfo.fileName = Path.GetRelativePath(referenceMaterialDir, filePath);
+
+                return materialInfo;
+            }
+            else
+            {
+                materialInfo.materials = new List<Material>();
+                return materialInfo;
+            }
+        }
+
+
+    }
+}
---
+++ src/Search.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static P5MatValidator.Program;
+using static P5MatValidator.Utils;
+using static GFDLibrary.Api.FlatApi;
+using GFDLibrary.Materials;
+using GFDLibrary;
+
+namespace P5MatValidator
+{
+    internal static class Search
+    {
+        internal async static void SearchForMaterial(string[] args)
+        {
+            string dumpPath = args[0];
+            List<string> matches = new();
+
+            string[] fileExtenstions = { "*.gmtd", "*.gmt", "*.GFS", "*.GMD" };
+            List<string> matFileNames = GetFiles($"{dumpPath}", fileExtenstions, SearchOption.AllDirectories);
+
+            List<MaterialInfo> referenceMaterials = new();
+
+            foreach (string matFile in matFileNames)
+            {
+                try
+                {
+                    referenceMaterials.Add(await GenerateMaterialList(matFile, dumpPath));
+                }
+                catch
+                {
+                    FailedMaterialFiles.Add(Path.GetRelativePath(dumpPath, matFile));
+                }
+            }
+
+            foreach (MaterialInfo materials in referenceMaterials)
+            {
+                bool doesMaterialHaveMatchingMember = false;
+
+                foreach (Material material in materials.materials)
+                {
+                    for (int i = 2; i < args.Length; i += 2)
+                    {
+                        doesMaterialHaveMatchingMember = CompareMaterialMemberValue(args[i], args[i + 1], material);
+
+                        if (!doesMaterialHaveMatchingMember) break;
+                    }
+
+                    if (doesMaterialHaveMatchingMember)
+                    {
+                        try
+                        {
+                            matches.Add($"{materials.fileName} -> {material.Name}");
+                        }
+                        catch (NullReferenceException e)
+                        {
+                            Console.WriteLine($"Skipping Material: {e}");
+                        }
+                    }
+                }
+                
+            }
+
+            if ( matches.Count > 0 )
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\nMatches Found ({matches.Count})");
+                Console.WriteLine("=====================================");
+                foreach (string match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+                Console.WriteLine("=====================================");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\nNo Results Found.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        static bool CompareMaterialMemberValue(string materialMember, string value, Material material)
+        {
+            //Material Flags
+            if (materialMember.ToLower() == "name")
+                return material.Name.ToLower() == value.ToLower();
+            if (materialMember.ToLower() == "bit0")
+                return material.Flags.HasFlag(MaterialFlags.Bit0) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit1")
+                return material.Flags.HasFlag(MaterialFlags.Bit1) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit2")
+                return material.Flags.HasFlag(MaterialFlags.Bit2) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit3")
+                return material.Flags.HasFlag(MaterialFlags.Bit3) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "enablevertcolors" || materialMember.ToLower() == "bit4")
+                return material.Flags.HasFlag(MaterialFlags.EnableVertColors) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "opaquealpha1" || materialMember.ToLower() == "bit5")
+                return material.Flags.HasFlag(MaterialFlags.OpaqueAlpha1) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit6")
+                return material.Flags.HasFlag(MaterialFlags.Bit6) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "enablelight" || materialMember.ToLower() == "bit7")
+                return material.Flags.HasFlag(MaterialFlags.EnableLight) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit8")
+                return material.Flags.HasFlag(MaterialFlags.Bit8) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit9")
+                return material.Flags.HasFlag(MaterialFlags.Bit9) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit10")
+                return material.Flags.HasFlag(MaterialFlags.Bit10) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "enablelight2" || materialMember.ToLower() == "bit11")
+                return material.Flags.HasFlag(MaterialFlags.EnableLight2) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "purplewireframe" || materialMember.ToLower() == "bit12")
+                return material.Flags.HasFlag(MaterialFlags.PurpleWireframe) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "opaquealpha2" || materialMember.ToLower() == "bit13")
+                return material.Flags.HasFlag(MaterialFlags.OpaqueAlpha2) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "receiveshadow" || materialMember.ToLower() == "bit14")
+                return material.Flags.HasFlag(MaterialFlags.ReceiveShadow) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "castshadow" || materialMember.ToLower() == "bit15")
+                return material.Flags.HasFlag(MaterialFlags.CastShadow) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hasattributes" || materialMember.ToLower() == "bit16")
+                return CheckAttribute(material, UInt32.Parse(value));
+            if (materialMember.ToLower() == "hasoutline" || materialMember.ToLower() == "bit17")
+                return material.Flags.HasFlag(MaterialFlags.HasOutline) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit18")
+                return material.Flags.HasFlag(MaterialFlags.Bit18) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "disablebloom" || materialMember.ToLower() == "bit19")
+                return material.Flags.HasFlag(MaterialFlags.DisableBloom) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hasdiffusemap" || materialMember.ToLower() == "bit20")
+                return material.Flags.HasFlag(MaterialFlags.HasDiffuseMap) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hasnormalmap" || materialMember.ToLower() == "bit21")
+                return material.Flags.HasFlag(MaterialFlags.HasNormalMap) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hasspecularmap" || materialMember.ToLower() == "bit22")
+                return material.Flags.HasFlag(MaterialFlags.HasSpecularMap) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hasreflectionmap" || materialMember.ToLower() == "bit23")
+                return material.Flags.HasFlag(MaterialFlags.HasReflectionMap) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hashighlightmap" || materialMember.ToLower() == "bit24")
+                return material.Flags.HasFlag(MaterialFlags.HasHighlightMap) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hasglowmap" || materialMember.ToLower() == "bit25")
+                return material.Flags.HasFlag(MaterialFlags.HasGlowMap) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hasnightmap" || materialMember.ToLower() == "bit26")
+                return material.Flags.HasFlag(MaterialFlags.HasNightMap) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hasdetailmap" || materialMember.ToLower() == "bit27")
+                return material.Flags.HasFlag(MaterialFlags.HasDetailMap) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "hasshadowmap" || materialMember.ToLower() == "bit28")
+                return material.Flags.HasFlag(MaterialFlags.HasShadowMap) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit29")
+                return material.Flags.HasFlag(MaterialFlags.Bit29) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit30")
+                return material.Flags.HasFlag(MaterialFlags.Bit30) == (UInt32.Parse(value) != 0);
+            if (materialMember.ToLower() == "bit31")
+                return material.Flags.HasFlag(MaterialFlags.Bit31) == (UInt32.Parse(value) != 0);
+
+            //other values
+            if (materialMember.ToLower() == "reflectivity" || materialMember.ToLower() == "field40")
+                return material.Field40 == float.Parse(value);
+            if (materialMember.ToLower() == "outlineindex" || materialMember.ToLower() == "field44")
+                return material.Field44 == float.Parse(value);
+            if (materialMember.ToLower() == "drawmethod" || materialMember.ToLower() == "field48")
+                return material.DrawMethod == (MaterialDrawMethod)UInt32.Parse(value);
+            if (materialMember.ToLower() == "field49")
+                return material.Field49 == UInt32.Parse(value);
+            if (materialMember.ToLower() == "field4a")
+                return material.Field4A == UInt32.Parse(value);
+            if (materialMember.ToLower() == "field4b")
+                return material.Field4B == UInt32.Parse(value);
+            if (materialMember.ToLower() == "field4c")
+                return material.Field4C == UInt32.Parse(value);
+            if (materialMember.ToLower() == "hightlightmapblendmode" || materialMember.ToLower() == "field4d")
+                return material.Field4D == UInt32.Parse(value);
+            if (materialMember.ToLower() == "field90")
+                return material.Field90 == UInt32.Parse(value);
+            if (materialMember.ToLower() == "field94")
+                return material.Field94 == UInt32.Parse(value);
+            if (materialMember.ToLower() == "field96")
+                return material.Field96 == UInt32.Parse(value);
+            if (materialMember.ToLower() == "field5c")
+                return material.Field5C == UInt32.Parse(value);
+            if (materialMember.ToLower() == "texcoord1" || materialMember.ToLower() == "field6c")
+                return material.Field6C == ParseTexcoord(value);
+            if (materialMember.ToLower() == "texcoord2" || materialMember.ToLower() == "field70")
+                return material.Field70 == ParseTexcoord(value);
+            if (materialMember.ToLower() == "disablebackfaceculling")
+                return material.DisableBackfaceCulling == UInt32.Parse(value);
+            if (materialMember.ToLower() == "field98")
+                return material.Field98 == UInt32.Parse(value);
+
+            Console.WriteLine($"Material member \"{materialMember}\" is not a valid member");
+            return false;
+        }
+        static UInt32 ParseTexcoord(string hexString)
+        {
+            if (hexString.StartsWith("0x"))
+            {
+                hexString = hexString.Substring(2);
+                return UInt32.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
+            }
+            else
+            {
+                return UInt32.Parse(hexString);
+            }
+        }
+
+        static bool CheckAttribute(Material material, UInt32 type)
+        {
+            if (!material.Flags.HasFlag(MaterialFlags.HasAttributes))
+                return false;
+
+            foreach (var attr in material.Attributes)
+            {
+                if (type == 0 && attr.AttributeType == MaterialAttributeType.Type0)
+                    return true;
+                if (type == 1 && attr.AttributeType == MaterialAttributeType.Type1)
+                    return true;
+                if (type == 2 && attr.AttributeType == MaterialAttributeType.Type2)
+                    return true;
+                if (type == 3 && attr.AttributeType == MaterialAttributeType.Type3)
+                    return true;
+                if (type == 4 && attr.AttributeType == MaterialAttributeType.Type4)
+                    return true;
+                if (type == 5 && attr.AttributeType == MaterialAttributeType.Type5)
+                    return true;
+                if (type == 6 && attr.AttributeType == MaterialAttributeType.Type6)
+                    return true;
+                if (type == 7 && attr.AttributeType == MaterialAttributeType.Type7)
+                    return true;
+                if (type == 8 && attr.AttributeType == MaterialAttributeType.Type8)
+                    return true;
+            }
+
+            return false;
+
+        }
+    }
+}
